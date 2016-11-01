ea211bde052444bf0bdc5acf08be1bd6f193c5ee
Joseph McGovern <joseph.mcgovern@webfilings.com>
2016-11-01 15:43:53 -0500

    delete items in categories

--- MyPantry/CategoryItemsTableViewController.swift
+++ MyPantry/CategoryItemsTableViewController.swift
@@ -15,11 +15,6 @@ class CategoryItemsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.topItem?.title = category?.name
-
-//        category = category
-//        if let category = category {
-//            print(category.name)
-//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -69,25 +64,24 @@ class CategoryItemsTableViewController: UITableViewController {
             category?.save()
         }
     }
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            category?.removePantryItemAt(index: indexPath.row)
+            category?.save()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
